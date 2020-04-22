282d6455724f3defcf5cbb51bb096d22bbf55662
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2020-04-22 03:30:17 -0500

    Iterador Nulo

---
+++ src/main/java/mx/unam/ciencias/myp/IteradorNulo.java
@@ -0,0 +1,23 @@
+package mx.unam.ciencias.myp;
+
+/**
+ * Clase que define el comportamiento del iterador nulo.
+ */
+public class IteradorNulo implements Iterador {
+
+    /**
+     * Nos dice si quedan elementos por recorrer en la lista.
+     * @return true si hay un elemento siguiente, false en otro caso.
+     */
+    public boolean hasNext() {
+        return false;
+    }
+
+    /**
+     * Nos da elemento siguiente.
+     * @return el elemento siguiente.
+     */
+    public T next() {
+        return null;
+    }
+}
