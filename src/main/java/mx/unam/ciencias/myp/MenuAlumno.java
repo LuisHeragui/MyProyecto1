1cca293cf834afe507aadfa399588cbe8c9fea47
DjEgo Rios <DjegoTCK>
2020-04-22 06:20:02 -0500

    Adición de lo menus.

---
+++ src/main/java/mx/unam/ciencias/myp/MenuAlumno.java
@@ -0,0 +1,14 @@
+package mx.unam.ciencias.myp;
+
+/**
+ * Clase que asigna el comportamiento del menu de alumnos.
+ */
+public class MenuAlumno implements Menu {
+
+	/**
+	 * Imprime el menu de los alumnos. 
+	 */
+	public void imprimirMenu () {
+		//Aqui va tu código.
+	}
+}
---
+++ src/main/java/mx/unam/ciencias/myp/MenuProfesor.java
@@ -0,0 +1,14 @@
+package mx.unam.ciencias.myp;
+
+/**
+ * Clase que asigna el comportamiento del menu de profesores.
+ */
+public class MenuProfesor implements Menu {
+
+	/**
+	 * Imprime el menu de los profesores. 
+	 */
+	public void imprimirMenu () {
+		//Aqui va tu código.
+	}
+}
