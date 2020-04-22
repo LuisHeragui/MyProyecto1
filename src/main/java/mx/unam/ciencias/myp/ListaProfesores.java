8ff99395a27b2ffc5f0c086cd7b6d7ba333fbb21
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2020-04-22 03:19:38 -0500

    Esqueleto ListaProfesores

---
+++ src/main/java/mx/unam/ciencias/myp/ListaProfesores.java
@@ -0,0 +1,50 @@
+package mx.unam.ciencias.myp;
+
+public class ListaProfesores implements ComponenteLista {
+
+    /* El máximo número de profesores que puede tener la lista. */
+    private static final int MAX_ITEMS = 20;
+    /* El número de profesores en la lista de profesores. */
+    private int numeroDeProfesores = 0;
+    /* La lista de alumnos. */
+    private Docente[] listaProfesores;
+
+    /**
+     * Constructor que define el estado incial de la lista de alumnos.
+     */
+    public ListaProfesores() {
+        listaProfesores = new Docente[MAX_ITEMS];
+    }
+
+    /**
+     * Agrega un componente al componente actual.
+     * @param componente el componente a agregar.
+     */
+    public void agrega(ComponenteLista componente) {
+        //Aquí va el código.
+    }
+
+    /**
+     * Elimina un componente del componente actual.
+     * @param componente el componente a eliminar.
+     */
+    public void elimina(ComponenteLista componente) {
+        //Aquí va el código.
+    }
+
+    /**
+     * Regresa una representación en cadena del componente.
+     * @return una representación en cadena del componente.
+     */
+    public String toString() {
+        //Aquí va el código.
+    }
+
+    /**
+     * Regresa un iterador para poder iterar al componente.
+     * @return un iterador para iterar al componente.
+     */
+    public Iterador creaIterador() {
+        //Aquí va el código.
+    }
+}
